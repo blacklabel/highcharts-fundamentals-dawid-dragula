b2b82e2888ea59b04706f4c484d1b2f4835e277f
Dawid Draguła <dawid.dragula@gmail.com>
2022-10-06 19:13:48 +0200

    Added buttons, edited gridlines

--- highcharts-api/highcharts/3-stacked-bar/main.js
+++ highcharts-api/highcharts/3-stacked-bar/main.js
@@ -3,95 +3,102 @@ const data = Array.from(
   () => Math.round(Math.random() * 15) * 5
 );
 
-Highcharts.chart("container", {
+Highcharts.chart('container', {
   chart: {
-    type: "bar",
+    type: 'bar',
     spacingTop: 50,
     events: {
       load: function () {
         this.customLabels = [];
         this.customLabels.push(
-          this.renderer
-            .text()
-            .attr({
-              text: "Issue",
-              "font-weight": "bold",
-              x: 10,
-              y: this.spacing[0] - 16,
-            })
-            .add(),
-
-          this.renderer
-            .text()
-            .attr({
-              text: "Record Count",
-              "font-weight": "bold",
-              x: this.plotLeft + 10,
-              y: this.spacing[0] - 16,
-            })
-            .add(),
-
-          this.renderer
-            .text()
-            .attr({
-              text: "Action",
-              "font-weight": "bold",
-              x: this.plotLeft + 200,
-              y: this.spacing[0] - 16,
-            })
-            .add()
+          this.renderer.text().attr({
+              text: 'Issue',
+              'font-weight': 'bold',
+              'text-anchor': 'end',
+              x: this.plotLeft - 12,
+              y: this.spacing[0] - 16
+            }).add(),
+          this.renderer.text().attr({
+              text: 'Record Count',
+              'font-weight': 'bold',
+              x: this.plotLeft,
+              y: this.spacing[0] - 16
+            }).add(),
+          this.renderer.text().attr({
+              text: 'Action',
+              'font-weight': 'bold',
+              x: this.yAxis[0].toPixels(300),
+              y: this.spacing[0] - 16
+            }).add()
         );
+        
+        Object.values(this.xAxis[0].ticks).forEach(tick => {
+          if (tick.pos > -1) {
+            tick.button = this.renderer.button('How to fix',
+            null, this.xAxis[0].toPixels(tick.pos + 0.45, true),
+            () => alert(`click ${tick.pos}`),
+            { 'stroke': 'blue', 'stroke-width': 3 }
+          ).add();
+          }
+        });
       },
-      redraw: function () {
-        console.log("redraw");
-      },
+      render: function () {
+        this.customLabels[2].attr({ x: this.plotSizeY - this.xAxis[0].ticks[0].button.getBBox().x });
+
+        Object.values(this.xAxis[0].ticks).forEach(tick => {
+          if (tick.pos > -1) {
+            tick.button.attr({ x: this.plotSizeY - tick.button.getBBox().x });
+          }
+
+          tick.gridLine.pathArray[0][1] = this.spacingBox.x;
+          tick.gridLine.attr({
+            'd': [...tick.gridLine.pathArray]
+          });
+        });
+      }
     },
   },
   title: {
-    text: "",
+    text: ''
   },
   xAxis: {
-    categories: ["Data", "Emails", "Duplicates", "Support"],
+    categories: ['Data', 'Emails', 'Duplicates', 'Support'],
     gridLineWidth: 1,
-    lineWidth: 0,
+    lineWidth: 0
   },
   yAxis: {
+    max: 310,
     title: {
-      text: "Amount",
+      text: 'Amount'
     },
     stackLabels: {
       enabled: true,
       formatter: function () {
         return `${this.total} K`;
-      },
+      }
     },
-    gridLineWidth: 0,
-    events: {
-      setExtremes: function () {
-        console.log("set extremes");
-      },
-    },
+    gridLineWidth: 0
   },
   legend: {
-    enabled: false,
+    enabled: false
   },
   plotOptions: {
     series: {
-      stacking: "normal",
+      stacking: 'normal'
     },
   },
   series: [
     {
-      data: data.slice(0, 4),
+      data: data.slice(0, 4)
     },
     {
-      data: data.slice(4, 8),
+      data: data.slice(4, 8)
     },
     {
-      data: data.slice(8, 12),
+      data: data.slice(8, 12)
     },
     {
-      data: data.slice(12, 16),
-    },
-  ],
+      data: data.slice(12, 16)
+    }
+  ]
 });
