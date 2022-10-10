ebe3d0e7719ed3e2705a2e4a16a15eebeea4746a
Dawid Draguła <dawid.dragula@gmail.com>
2022-10-10 09:14:45 +0200

    Added max point labels

--- highcharts-api/highcharts/6-visible-points-zoom/main.js
+++ highcharts-api/highcharts/6-visible-points-zoom/main.js
@@ -6,18 +6,46 @@ Highcharts.chart('container', {
     zoomType: 'xy',
     events: {
       load: function () {
-        this.customLabel = this.renderer.text().attr({ 'font-size': 14 }).add();
+        this.customLabel = this.renderer.text().attr({ 'font-size': 13 }).add();
+        this.maxPointLabels = [];
+        this.xAxisPoints = [];
       },
       render: function () {
-        const visiblePoints = this.series[0].points.filter(i => i.isInside == true);
+        const visiblePoints = this.series[0].points.filter(i => i.isInside == true),
+              maxVisibleY = Math.max(...visiblePoints.map(i => i.y)),
+              maxVisiblePoints = visiblePoints.filter(i => i.y == maxVisibleY);
+
+        this.maxPointLabels.forEach(i => i.destroy());
+        this.maxPointLabels.length = 0;
+
+        this.xAxisPoints.forEach(i => i.destroy());
+        this.xAxisPoints.length = 0;
 
         this.customLabel.attr({
           text: 'Visible points: ' + visiblePoints.length,
           x: this.plotLeft,
           y: 55 + this.plotHeight + this.plotTop
         });
+
+        maxVisiblePoints.forEach(point => {
+          this.maxPointLabels.push(
+            this.renderer
+              .text(point.y, point.plotX + this.plotLeft, point.plotY + this.plotTop - 10)
+              .attr({
+                'text-anchor': 'middle',
+                'fill': 'red',
+                'font-weight': 'bold',
+                zIndex: 1
+              })
+              .add()
+          );
+
+          this.xAxisPoints.push(
+
+          );
+        });
       }
     }
   },
-  series: [{ data }]
+  series: [{ data }],
 });
