b8c86988a23068076a6fcb626988ffb4d4e64de7
Dawid Draguła <dawid.dragula@gmail.com>
2022-10-10 10:29:05 +0200

    Added lines connecting columns

--- highcharts-api/highcharts/7-connecting-column-edges/main.js
+++ highcharts-api/highcharts/7-connecting-column-edges/main.js
@@ -7,10 +7,31 @@ Highcharts.chart('container', {
     type: 'column',
     events: {
       load: function () {
-
+        this.series.forEach(series => {
+          for (let i = 0; i < series.data.length - 1; i++) {
+            series.data[i].customLine = this.renderer.path().attr({ 'stroke-width': 1, zIndex: 1 }).add();
+          }
+        });
       },
       render: function () {
+        this.series.forEach(series => {
+          for (let i = 0; i < series.data.length - 1; i++) {
+            const data = series.data[i];
+            const nextData = series.data[i + 1];
 
+            data.customLine.attr({
+              'd': ['M', data.shapeArgs.x + data.shapeArgs.width + this.plotLeft, data.plotY + this.plotTop,
+                    'L', nextData.shapeArgs.x + this.plotLeft, nextData.plotY + this.plotTop, 'z'],
+              'stroke': data.color,
+            });
+
+            if (series.visible) {
+              data.customLine.show();
+            } else {
+              data.customLine.hide();
+            }
+          }
+        });
       }
     }
   },
