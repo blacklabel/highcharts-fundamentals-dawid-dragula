da81c121b5dc65e599e1d74597ac796ee8ed4845
Dawid Draguła <dawid.dragula@gmail.com>
2022-10-12 11:40:55 +0200

    Finished the exercise

---
+++ highcharts-api/highcharts/13-synchronized-pies/main.js
@@ -0,0 +1,63 @@
+const data = [{
+  name: 'Commerce', y: 74.3,
+}, {
+  name: 'Engineering', y: 32.3
+}, {
+  name: 'Financial Services', y: 18.8
+}, {
+  name: 'Logistics, Aviation & Shipping', y: 5.5
+}, {
+  name: 'Seafood & Marine', y: 9.2
+}, {
+  name: 'Corporate Services & others', y: 1.2
+}];
+
+Highcharts.chart('container', {
+  chart: {
+    type: 'pie'
+  },
+  tooltip: {
+    followPointer: false
+  },
+  series: [{
+    data,
+    center: ['30%'],
+    showInLegend: true
+  }, {
+    data,
+    center: ['70%']
+  }],
+  plotOptions: {
+    pie: {
+      dataLabels: {
+        enabled: false
+      },
+      point: {
+        events: {
+          mouseOver: function () {
+            const point = this,
+                  chart = point.series.chart,
+                  oppositeSeries = chart.series[Number(!point.series.index)];
+
+            oppositeSeries.points[point.index].setState('hover');
+
+            if (!chart.tooltip2) {
+              chart.tooltip2 = new Highcharts.Tooltip(chart, chart.tooltip.options);
+            }
+
+            chart.tooltip2.refresh(oppositeSeries.data[point.index]);
+          },
+          mouseOut: function () {
+            this.series.chart.tooltip2.hide();
+          },
+          legendItemClick: function () {
+            const point = this
+                  oppositeSeries = point.series.chart.series[Number(!point.series.index)];
+            
+            oppositeSeries.points[point.index].setVisible(!point.visible);
+          }
+        }
+      }
+    }
+  }
+});
