a25da75a0e3756fb076b4b6fc7bc3a7fc7139b99
Dawid Draguła <dawid.dragula@gmail.com>
2022-10-12 20:28:45 +0200

    Finished the exercise

--- highcharts-api/highcharts-gantt/1-gantt-axes-groups/main.js
+++ highcharts-api/highcharts-gantt/1-gantt-axes-groups/main.js
@@ -1,20 +1,30 @@
 Highcharts.ganttChart('container', {
-
+    xAxis: [{
+        visible: false
+    }, {
+        labels: {
+            format: '{value:%A, %d %b, %Y}'
+        },
+        tickInterval: 432000000
+    }],
+    yAxis: {
+        categories: ['Main', 'First', 'Second']
+    },
     series: [{
         name: 'Project 1',
         data: [{
-            name: 'Main',
+            y: 0,
             pointWidth: 0
         }, {
-            name: 'First',
+            y: 1,
             start: 1560902400000,
             end: 1561075200000,
         }, {
-            name: 'Second',
+            y: 2,
             start: 1560902400000,
             end: 1561075200000
         }, {
-            name: 'Second',
+            y: 2,
             start: 1561507200000,
             end: 1561680000000
         }]
