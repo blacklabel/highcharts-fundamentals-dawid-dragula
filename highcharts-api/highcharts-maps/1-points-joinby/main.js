b4ab63ef3611de856c62ec44e342504585166104
Dawid Draguła <dawid.dragula@gmail.com>
2022-10-12 21:14:24 +0200

    Finished the exercise

--- highcharts-api/highcharts-maps/1-points-joinby/main.js
+++ highcharts-api/highcharts-maps/1-points-joinby/main.js
@@ -1,14 +1,22 @@
 Highcharts.mapChart('container', {
-
+    chart: {
+        map: Highcharts.maps["custom/world"],
+    },
     series: [{
         data: [
-            ['POL', 100],
-            ['USA', 90],
-            ['PER', 50],
-            ['TZA', 40],
-            ['AUS', 1]
-        ],
-        joinBy: ['iso-a3']
+            ['pl', 100],
+            ['us', 90],
+            ['pe', 50],
+            ['tz', 40],
+            ['au', 1]
+        ]
+    }, {
+        type: 'mapbubble',
+        data: [
+            ['nz', 3],
+            ['tz', 2],
+            ['no', 1],
+            ['ca', 4]
+        ]
     }]
-
 });
