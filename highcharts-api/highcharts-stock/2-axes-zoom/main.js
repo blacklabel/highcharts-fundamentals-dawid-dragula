6913c90822ab40f4db1981c208bf00c193165640
Dawid Draguła <dawid.dragula@gmail.com>
2022-10-20 11:49:48 +0200

    Done: c

--- highcharts-api/highcharts-stock/2-axes-zoom/main.js
+++ highcharts-api/highcharts-stock/2-axes-zoom/main.js
@@ -1,25 +1,25 @@
+function centerZoom(axis, strength) {
+  if (axis.center === undefined) {
+    axis.zoomOffset = (axis.max - axis.min) / 2;
+    axis.center = axis.min + axis.zoomOffset;
+  }
+
+  axis.zoomOffset += strength * axis.toValue(1);
+
+  if (axis.zoomOffset < 0) {
+    axis.zoomOffset = 0;
+  }
+
+  axis.setExtremes(axis.center - axis.zoomOffset, axis.center + axis.zoomOffset, false);
+  axis.chart.redraw(false);
+}
+
 function onMouseMove(yAxes, event) {
   yAxes.forEach(yAxis => {
     if (yAxis.drag) {
-      if (yAxis.center === undefined) {
-        yAxis.dragOffset = (yAxis.max - yAxis.min) / 2;
-        yAxis.center = yAxis.min + yAxis.dragOffset;
-      }
-  
-      yAxis.dragOffset += event.movementY * yAxis.toValue(1) * 0.001;
-  
-      if (yAxis.dragOffset < 0) {
-        yAxis.dragOffset = 0;
-      }
-  
-      yAxis.setExtremes(yAxis.center - yAxis.dragOffset, yAxis.center + yAxis.dragOffset, false);
-      yAxis.chart.redraw(false);
+      centerZoom(yAxis, event.movementY * 0.001);
     }
   })
-}
-
-function onMouseDown(yAxis) {
-  yAxis.drag = true;
 }
 
 function onMouseUp(yAxes) {
@@ -27,16 +27,18 @@ function onMouseUp(yAxes) {
 }
 
 function onWheel(xAxis, event) {
-  if (xAxis.canDrag) {
-    let dragOffset = event.deltaY * xAxis.toValue(1) * 0.000001;
+  if (xAxis.scaleMode === 'max') {
+    let zoomOffset = event.deltaY * xAxis.toValue(1) * 0.000001;
     const firstX = Math.min(...xAxis.series.map(s => s.data[0].x));
 
-    if (xAxis.min + dragOffset < firstX) {
-      dragOffset = firstX - xAxis.min;
+    if (xAxis.min + zoomOffset < firstX) {
+      zoomOffset = firstX - xAxis.min;
     }
 
-    xAxis.setExtremes(xAxis.min + dragOffset, xAxis.max, false);
+    xAxis.setExtremes(xAxis.min + zoomOffset, xAxis.max, false);
     xAxis.chart.redraw(false);
+  } else if (xAxis.scaleMode === 'center') {
+    centerZoom(xAxis, -event.deltaY * 0.000002);
   }
 }
 
@@ -46,30 +48,37 @@ Highcharts.getJSON('https://demo-live-data.highcharts.com/aapl-c.json', data => 
       events: {
         load: async function () {
           const chart = this;
-                yAxes = chart.yAxis,
-                xAxis = chart.xAxis[0],
-                labelGroup = chart.yAxis[0].labelGroup,
-                labelGroupBBox = labelGroup.getBBox();
-          
+                yAxes = chart.yAxis.filter(p => p.userOptions.id !== 'navigator-y-axis'),
+                xAxis = chart.xAxis[0];
+
           yAxes.forEach(yAxis => {
-            yAxis.axisRect = chart.renderer.rect().attr({ width: 30, height: yAxis.height, fill: 'transparent'}).css({ cursor: 'n-resize' }).add(labelGroup);
+            yAxis.axisRect = chart.renderer.rect().attr({ width: 30, height: yAxis.height, fill: 'transparent'}).css({ cursor: 'n-resize' }).add(yAxis.labelGroup);
   
-            yAxis.axisRect.on('mousedown', () => onMouseDown(yAxis));
-            yAxis.axisRect.on('mouseover', () =>  { xAxis.canDrag = true });
-            yAxis.axisRect.on('mouseout', () =>  { xAxis.canDrag = false });
+            yAxis.axisRect.on('mousedown', () => { yAxis.drag = true; });
+            yAxis.axisRect.on('mouseover', () =>  { xAxis.scaleMode = 'max'; });
+            yAxis.axisRect.on('mouseout', () =>  { xAxis.scaleMode = null; });
           });
 
           document.addEventListener('mousemove', event => onMouseMove(yAxes, event));
           document.addEventListener('mouseup', () => onMouseUp(yAxes));
           document.addEventListener('wheel', event => onWheel(xAxis, event));
+
+
+          xAxis.axisRect = chart.renderer.rect().attr({ fill: 'transparent' }).css({ cursor: 'w-resize' }).add();
+
+          Highcharts.addEvent(xAxis.labelGroup.element, 'mouseover', () => { xAxis.scaleMode = 'center' });
+          Highcharts.addEvent(xAxis.labelGroup.element, 'mouseout', () => { xAxis.scaleMode = 'center' });
+          xAxis.axisRect.on('mouseover', () => { xAxis.scaleMode = 'max' });
+          xAxis.axisRect.on('mouseout', () => { xAxis.scaleMode = null });
         },
         render: async function () {
           const chart = this,
-                yAxes = chart.yAxis;
+                yAxes = chart.yAxis.filter(p => p.userOptions.id !== 'navigator-y-axis');
           
           yAxes.forEach(yAxis => {
             yAxis.axisRect.attr({ x: yAxis.width - 10, y: yAxis.top }).toFront();
           });
+          xAxis.axisRect.attr(xAxis.labelGroup.getBBox());
         }
       }
     },
